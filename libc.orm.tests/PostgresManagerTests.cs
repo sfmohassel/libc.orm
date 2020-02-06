a7ba04df6ecaf3d894f7ccd64bbdc48e8aeb7a0e
Saeid Farahi <saeidfarahi@hotmail.com>
2020-02-06 14:22:28 +0330

    some tests added

---
+++ libc.orm.tests/PostgresManagerTests.cs
@@ -0,0 +1,15 @@
+﻿using libc.orm.postgres.Management;
+using Microsoft.Extensions.Logging.Abstractions;
+using Npgsql;
+using Xunit;
+namespace libc.orm.tests {
+    public class PostgresManagerTests {
+        [Fact]
+        public void GetDatabaseNames() {
+            var cstr = "Server=localhost;Port=5432;Uid=postgres;Pwd=Admin!@#;CommandTimeout=20;Timeout=3;";
+            var cstrb = new NpgsqlConnectionStringBuilder(cstr);
+            var manager = new PostgresManager(NullLogger.Instance, cstrb);
+            var databases = manager.GetAllDatabaseNames();
+        }
+    }
+}
